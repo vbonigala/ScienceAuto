1801da8fb3223fcddd481bdbb4df9abeaafaaac0
vbonigala <bonigala.testing@gmail.com>
2016-09-04 14:02:48 -0400

    first commit

---
+++ src/main/java/com/viji/test/SciTest.java
@@ -0,0 +1,29 @@
+package com.viji.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SciTest {
+
+	public static void main(String[] args) throws Exception {
+		FirefoxDriver wd;
+		wd = new FirefoxDriver();
+		wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		wd.get("https://www.google.com/");
+		wd.findElement(By.id("lst-ib")).click();
+		wd.findElement(By.id("lst-ib")).clear();
+		wd.findElement(By.id("lst-ib")).sendKeys("science");
+		wd.findElement(By.name("btnG")).click();
+		wd.findElement(By.linkText("Science - Wikipedia, the free encyclopedia")).click();
+		if (!wd.findElement(By.id("firstHeading")).getText().equals("Science")) {
+			System.out.println("verifyText failed");
+		} else {
+			System.out.println("verifyText passed");
+			
+		}
+		wd.quit();
+	}
+
+}
